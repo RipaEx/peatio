c4054531db7b0d0a83264c30ea36498897e74f93
Giovanni Silvestri <gsit80@gmail.com>
2019-04-25 19:09:58 +0200

    Redirect: welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,5 +6,10 @@ class WelcomeController < ApplicationController
   include Concerns::DisableCabinetUI
 
   def index
+    if ENV['URL_HOST']
+      redirectWelcome = ENV.fetch('URL_SCHEME') + '://' + ENV.fetch('URL_HOST') + '/auth/barong'
+      Rails.logger.info('redirectWelcome: ' + redirectWelcome)
+      redirect_to redirectWelcome
+    end  
   end
 end
